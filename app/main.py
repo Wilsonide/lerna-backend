2d663767078e4ff7acc4ee63b795780f6d4da10f
Wilson Icheku <ichekuwilson538@gmail.com>
2026-05-21 15:49:26 +0100

    setup

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ from app.api import (
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://lerna-frontend-fvvw.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
